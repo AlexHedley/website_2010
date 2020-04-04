44f12c0bc0f5667198552bb900ebb225fd7d68c2
Alex Hedley <alex.hedley@me.com>
2020-04-04 12:34:35 +0100

    Start of doodle god

---
+++ doodleGod/showhide.js
@@ -0,0 +1,10 @@
+function hide(id)
+{
+	var elem = document.getElementById(id);
+	elem.style.display = 'none';
+}
+function show(id)
+{
+	var elem = document.getElementById(id);
+	elem.style.display = 'block';
+}
